2aa7ddad84608ddecec3c10251bcd9f35866fbca
Jonatan Martín Merinero <jmmerinero@gmail.com>
2015-06-22 09:39:33 +0200

    Busy waiting added. [G]

--- features/calabash/calabash_methods.rb
+++ features/calabash/calabash_methods.rb
@@ -1,14 +1,16 @@
 module CalabashMethods
   def press(element)
+    wait_for_element_exists(@list_of_elements[element][@platform][@env], :timeout => 10)
     touch(@list_of_elements[element][@platform][@env])
   end
 
   def fill(element, text)
+    wait_for_element_exists(@list_of_elements[element][@platform][@env], :timeout => 10)
     clear_text_in(@list_of_elements[element][@platform][@env])
     enter_text(@list_of_elements[element][@platform][@env], text)
   end
 
   def exists?(element)
-    query(element)
+    wait_for_element_exists(@list_of_elements[element][@platform][@env], :timeout => 10)
   end
 end
